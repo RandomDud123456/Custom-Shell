efddabf65fc281865bbb0f3982b605c313bef09c
RandomDud123456 <112799199+RandomDud123456@users.noreply.github.com>
2024-07-06 16:28:28 +0530

    Delete mini-project-1-RandomDud123456-main directory

--- mini-project-1-RandomDud123456-main/prompt.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PROMPT_H4_
-#define __PROMPT_H4_
-
-#include "rem_fun.h"
-#include "headers.h"
-void print_prompt(const char *username, const char *systemname, const char *current_dir, char *home_dir);
-
-#endif
